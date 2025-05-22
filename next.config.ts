4e589c4fb210880b836529fa5c63796645ba0237
sctrailblazer <sctrailblazer4x4@gmail.com>
2025-05-22 10:10:05 -0400

    Initial Commit

---
+++ next.config.ts
@@ -0,0 +1,31 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export', // Add this line for static site generation
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true, // Add this line to disable image optimization for static export
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
